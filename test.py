22eea91fe4f5a6275ee29263c5933229216b8c7b
ac1224 <andoridthac@gmail.com>
2022-09-02 22:57:39 +0900

    gitignore

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # %%
+import torch
 from diffusers import DiffusionPipeline
 
 model_id = "CompVis/ldm-text2im-large-256"
@@ -6,11 +7,18 @@ model_id = "CompVis/ldm-text2im-large-256"
 # load model and scheduler
 ldm = DiffusionPipeline.from_pretrained(model_id)
 
+# device = "cuda"
+# ldm = ldm.to(device)
+# %%
 # run pipeline in inference (sample random noise and denoise)
-prompt = "A painting of a squirrel eating a burger"
+prompt = "総書記, ボタン, missile"
 images = ldm([prompt], num_inference_steps=50, eta=0.3, guidance_scale=6)["sample"]
 
 # save images
+
 for idx, image in enumerate(images):
-    image.save(f"squirrel-{idx}.png")
+    image.save(f"{prompt.split(' ')[0]}-{idx}.png")
+# %%
+
+
 # %%
